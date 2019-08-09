83d010d20cb2447e91a4cd30b1b13993b773743e
Guillaume Pouyat <guiforge@gmail.com>
2019-08-09 16:46:38 +0200

    WIP download

---
+++ src/controllers/download.js
@@ -0,0 +1,7 @@
+const logger = require('../utils/logger');
+
+function checkAuth(id, check)
+
+function download(step) {
+
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,20 +1,39 @@
-const controllers = require('../controllers/controllers');
+const { upload } = require('../controllers/controllers');
+const { Download } = require('../controllers/controllers');
 // const logger = require('../utils/logger')
 
 const initEndpoints = app => {
-  app.get('/', (req, res) => {
-    res.send('homa');
-  });
-
   app.post('/upload', async (req, res) => {
     try {
-      const id = await controllers.upload(req.body);
+      const id = await upload(req.body);
       res.status(200);
       res.send({ id });
     } catch (error) {
       res.status(400);
       res.send({ Error: 'Error' });
     }
+  });
+
+  app.get('/download/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const reg = /^[0-9a-fA-F]{24}$/;
+      if (!id.match(reg)) {
+        throw new Error('Error Bad format');
+      }
+
+
+      res.status(200);
+      res.send({ a: 'a' });
+    } catch (error) {
+      res.status(400);
+      res.send({ error: error.message });
+    }
+  });
+
+  app.get('/Download/*', async (req, res) => {
+    res.status(400);
+    res.send({ Error: 'Bad Request' });
   });
 };
 
