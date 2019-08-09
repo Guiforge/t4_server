45b7edc54fdea4e5433f5ab0b6f879d169a073de
Guillaume Pouyat <guiforge@gmail.com>
2019-08-09 16:46:00 +0200

    Upload no longer needs to transform objects into Array

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,23 +1,14 @@
 const models = require('../models/models.js');
 const logger = require('../utils/logger');
 
-function objToArray(obj) {
-  const arr = [];
-  Object.keys(obj).map(objectKey => {
-    arr.push(obj[Number(objectKey)]);
-  });
-  return arr;
-}
-
 const { Data } = models;
+
 module.exports = async function upload(data) {
   try {
     const dataObj = new Data(data);
-    dataObj.enc.file = objToArray(data.enc.file);
-    dataObj.enc.meta = objToArray(data.enc.meta);
-    dataObj.key = objToArray(data.key);
-    logger.debug(dataObj);
+    logger.debug(`prepare to save! ${dataObj.id}`);
     await dataObj.save();
+    logger.debug(`save ${dataObj.id}`);
     return dataObj.id;
   } catch (error) {
     logger.error(`Error upload: ${error}`);
