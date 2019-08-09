c4e8835e3bfb6ac3c3258d649dc0e7784378a7bb
Guillaume Pouyat <guiforge@gmail.com>
2019-08-09 16:44:58 +0200

    increase limit of upload

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(new RateLimit(config.rate));
 app.use(cors());
 app.use(compression());
 app.use(helmet());
-app.use(express.json({ limit: '300kb' }));
+app.use(express.json({ limit: '3gb' }));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
