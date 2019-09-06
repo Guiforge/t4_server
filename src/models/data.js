222e506d9d9ea13fed7597c63ce6a71a19009f2d
gpouyat <guiforge@gmail.com>
2019-09-06 16:22:32 +0200

    Update Data for new front

--- src/models/data.js
+++ src/models/data.js
@@ -1,35 +1,29 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 
-function arrayLimit(arr) {
-  return arr.length >= 10;
-}
+// function arrayLimit(arr) {
+//   return arr.length >= 10;
+// }
 
-function arrayLimitIV(arr) {
-  return arr.length === 12;
-}
+// function arrayLimitIV(arr) {
+//   return arr.length === 12;
+// }
 
 const dataShema = new mongoose.Schema({
   enc: {
-    meta: {
-      data: { type: [Number], required: true, validate: [arrayLimit, 'meta is too small'] },
-      ivMeta: {
-        type: [Number],
-        required: true,
-        validate: [arrayLimitIV, 'Ivmeta is not in correct format']
-      }
-    },
-    file: {
-      idFile: { Type: [Number] },
-      // data: { type: [Number], required: true, validate: [arrayLimit, 'meta is too small'] },
-      ivFile: {
-        type: [Number],
-        required: true,
-        validate: [arrayLimitIV, 'IvFile is not in correct format']
-      }
+    encrypted: { type: String, requires: true },
+    auth: {
+      type: Buffer,
+      required: true,
+      min: [arr => arr.byteLength === 16, 'Auth Tag Meta should have length of 16']
     }
   },
-  key: {
+  ivMeta: {
+    type: Buffer,
+    required: true,
+    min: [arr => arr.byteLength === 92, 'Vector Meta should have length of 92']
+  },
+  keyAuth: {
     type: [Number],
     required: true,
     min: [arr => arr.length === 31, 'key should have length of 31']
